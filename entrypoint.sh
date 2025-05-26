1ff880407666c921c963953a7e76bb4265e039d0
TeseySTD <skoreiko.mykhailo@chnu.edu.ua>
2025-05-26 21:55:23 +0300

    fix(render):add /app in destination.

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,8 +4,8 @@ set -e
 mkdir -p /app/config/cookies
 
 if [ -f /etc/secrets/cookies.txt ]; then
-  cp /etc/secrets/cookies.txt /config/cookies/cookies.txt
-  echo "✅ Copied cookies.txt to /config/cookies/"
+  cp /etc/secrets/cookies.txt /app/config/cookies/cookies.txt
+  echo "✅ Copied cookies.txt to /app/config/cookies/"
 else
   echo "⚠️ /etc/secrets/cookies.txt not found, skipping copy"
 fi
